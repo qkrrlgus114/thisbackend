1bef127904647092e262184da440bbc12469a1f1
qkrrlgus114 <qkrrlgus114@naver.com>
2024-04-29 18:29:13 +0900

    Feat : 테스트 컨트롤러

---
+++ park/src/main/java/com/thisspring/park/SimpleRestController.java
@@ -0,0 +1,18 @@
+package com.thisspring.park;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleRestController {
+
+    @RequestMapping("/")
+    public String hello(){
+        return "Hello";
+    }
+
+    @RequestMapping("bye")
+    public String bye(){
+        return "Bye";
+    }
+}
